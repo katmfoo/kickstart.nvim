7eb428dca85b2503b67a27b7df9cbbb1bad8056f
katmfoo <katmfoo@gmail.com>
2024-02-29 19:15:59 -0500

    Add essential language servers

--- init.lua
+++ init.lua
@@ -554,8 +554,9 @@ require('lazy').setup {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
+        tsserver = {},
+        intelephense = {},
+        angularls = {},
 
         lua_ls = {
           -- cmd = {...},
